0186abcdb602b88253958321161a63ecf39c44bf
Ilya Biryukov <ilya@goparty.ie>
2013-12-16 22:09:29 +0000

    Floor functions

---
+++ lib/DoctrineExtensions/Query/Mysql/Floor.php
@@ -0,0 +1,31 @@
+<?php
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode,
+Doctrine\ORM\Query\Lexer;
+
+class Floor
+{
+    private $number;
+    
+    /**
+     * @override
+     */
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return "FLOOR( " . $sqlWalker->walkArithmeticPrimary($this->number) . ")";
+    }
+
+    /**
+     * @override
+     */
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->number = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
